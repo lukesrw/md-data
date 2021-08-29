1fd6a9cf66c7b5153945e4a50ff616570e575489
lukesrw <lukesrw+github@gmail.com>
2021-08-29 15:48:19 +0100

    Added test/readme builder

---
+++ ts/test/index.ts
@@ -0,0 +1,28 @@
+import { readFile, writeFile } from "fs/promises";
+import { join } from "path";
+import { MDData } from "..";
+
+async function test() {
+    let readme = await readFile(join(__dirname, "..", "..", "ts", "test", "README.md"), "utf-8");
+
+    let md1 = await readFile(join(__dirname, "..", "..", "ts", "test", "test.md"), "utf-8");
+
+    let schema = MDData.fromMD(md1);
+
+    let md2 = await schema.toFile(join(__dirname, "test (copy).md"));
+
+    let json = await schema.toFile(join(__dirname, "test.json"));
+
+    let sql = await schema.toFile(join(__dirname, "test.sql"));
+
+    await writeFile(
+        join(__dirname, "..", "..", "README.md"),
+        readme
+            .replace("<MD1>", md1)
+            .replace("<MD2>", md2)
+            .replace("<JSON>", JSON.stringify(JSON.parse(json), null, 4))
+            .replace("<SQL>", sql)
+    );
+}
+
+test();
---
+++ ts/test/test.md
@@ -0,0 +1,16 @@
+# Headquarters (building)
+
+-   House Number: 221b
+-   Street: Baker Street
+-   City: London
+-   Postcode: NW1 6XE
+
+## Sherlock Holmes (occupant)
+
+-   Forename: Sherlock
+-   Surname: Holmes
+
+## John Watson (occupant)
+
+-   Forename: John
+-   Surname: Watson
